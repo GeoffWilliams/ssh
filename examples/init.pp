8cd2d1c29b7d636543abdd1c5a7e396ca35919b0
Geoff Williams <geoff@geoffwilliams.me.uk>
2018-06-14 11:09:37 +1000

    SSH module version 2.0
    Simplify the existing module by removing all code that deals with setting specific versions and move to in-module hiera data. This allows for full support of all `sshd_config`
    options without the need for special casing. We now only make the settings changes that the users instructs rather then picking a few safe options from `params.pp` (which is
    also gone anyway). This means users are only managing the settings they want to manage and avoid accidentally clobbering new settings from the vendor which may contain
    important security fixes. All settings for the config file are now passed in the parameter `settings`. This replaces the `extra_config` parameter and all of the individual
    settings parameters that used to exist. Since this is a breaking change the version number has been bumped and users of the previous module need to either change their
    profile/hiera usage or stay on the old version.

--- examples/init.pp
+++ examples/init.pp
@@ -1,2 +1,12 @@
 # @PDQTest
-include ssh
+class { "ssh":
+  settings => {
+    "Banner"              => "/etc/issue.net",
+    "Protocol"            => "2",
+    "MaxAuthTries"        => "4",
+    "PermitRootLogin"     => "no",
+    "Ciphers"             => ["aes128-ctr","aes192-ctr","aes256-ctr"],
+    "ClientAliveInterval" => "300",
+    "ClientAliveCountMax" => "1",
+  }
+}
--- manifests/init.pp
+++ manifests/init.pp
@@ -6,10 +6,14 @@
 # then this can be passed using `manual_package`. This is a requirement for AIX
 # and Solaris.
 #
-# Where configuration away from the defaults in this module is required, you may
-# choose to edit/not edit particular directives in the sshd_config config file
-# (uses Augeas). Note that this is an in-place edit and will preserve any
-# existing content that is not required to be changed.
+# This module only rewrites the parts the of the `sshd_config` file you tell it to
+# in order to preserve defaults/fixes from the vendor. Where explicit
+# configuration is required, it should be passed in the `settings` hash.
+#
+# Augeaus SSH provider is used to perform the re-write of the `sshd_config` file
+#
+# @see https://forge.puppet.com/herculesteam/augeasproviders_ssh
+# @see https://forge.puppet.com/herculesteam/augeasproviders_core
 #
 # @example Install SSH server using the defaults
 #   include ssh
@@ -26,8 +30,10 @@
 #
 # @example Install and configure SSH server but disable root logins and enable debug logging
 #   class { "ssh":
-#     permit_root_login => "no",
-#     log_level         => "DEBUG",
+#     settings => {
+#       "permit_root_login" => "no",
+#       "log_level"         => "DEBUG",
+#     },
 #   }
 #
 # @example Install and configure SSH server on AIX
@@ -38,6 +44,19 @@
 #       "source"       => "/tmp/megacorp_ssh.rpm"
 #     },
 #   }
+#
+# @example Hiera data for some common settings (supported keys are those allowed by ssh itself)
+#   ssh::settings:
+#     Banner: "/etc/issue.net"
+#     Protocol: "2"
+#     MaxAuthTries: "4"
+#     PermitRootLogin: "no"
+#     Ciphers:
+#       - "aes128-ctr"
+#       - "aes192-ctr"
+#       - "aes256-ctr"
+#     ClientAliveInterval: "300"
+#     ClientAliveCountMax: "1"
 #
 # @param manage_package `true` to manage package installation otherwise false
 # @param manage_service `true` to manage the service otherwise false
@@ -58,93 +77,22 @@
 # @param config_file_mode Permissions for SSHD config file
 # @param manage_service True if we should manage the SSHD service with Puppet
 # @param service_name Name of the SSHD service on this system
-# @param protocol `protocol` parameter to write to config file
-# @param manage_protocol `true` to manage the `protocol` line in config file
-#   otherwise leave alone
-# @param log_level `log_level` parameter to write to config file
-# @param manage_log_level `true` to manage `log_level` in config file otherwise
-#   leave alone
-# @param x11_forwarding `x11_forwarding` value for config file
-# @param manage_x11_forwarding `true` to manage `x11_forwarding` in config file
-#   otherwise leave alone
-# @param max_auth_tries `max_auth_tries` value for config file
-# @param manage_max_auth_tries `true` to manage `max_auth_tries` in config file
-#   otherwise leave alone
-# @param ignore_rhosts `ignore_rhosts` value for config file
-# @param manage_ignore_rhosts `true` to manage `ignore_rhosts` in config file
-#   otherwise leave alone
-# @param hostbased_authentication `hostbased_authentication` value for config file
-# @param manage_hostbased_authentication `true` to manage
-#   `hostbased_authentication` in config file otherwise leave alone
-# @param permit_root_login value for `permit_root_login` in config file
-# @param manage_permit_root_login `true` to manage `permit_root_login` in config
-#   file otherwise leave alone
-# @param permit_empty_passwords value for `permit_empty_passwords` in config
-#   file
-# @param manage_permit_empty_passwords `true` to manage `permit_empty_passwords`
-#   in config file otherwise leave alone
-# @param permit_user_environment value for `permit_user_environment` in config
-#   file
-# @param manage_permit_user_environment `true` to manage
-#   `permit_user_environment` in config file otherwise leave alone
-# @param ciphers value for `ciphers` in config file
-# @param manage_ciphers `true` to manage `ciphers` in config file otherwise
-#   leave alone
-# @param client_alive_interval value for `client_alive_interval` in config file
-# @param manage_client_alive_interval `true` to manage `client_alive_interval`
-#   in config file otherwise leave alone
-# @param client_alive_count_max value for `client_alive_count_max` in config
-#   file
-# @param manage_client_alive_count_max `true` to manage `client_alive_count_max`
-#   in config file otherwise leave alone
-# @param banner value for `banner` in config file
-# @param manage_banner `true` to manage `banner` in config file otherwise leave
-#   alone
-# @param extra_config hash of extra configuration to add to the config file, in
-#   a form suitable for `create_resources("sshd_config", $extra_config)`
 class ssh(
-    Boolean $manage_package                     = $ssh::params::manage_package,
-    Boolean $manage_service                     = $ssh::params::manage_service,
-    String $service_name                        = $ssh::params::service_name,
-    Enum["stopped", "running"] $service_ensure  = $ssh::params::service_ensure,
-    Boolean $service_enable                     = $ssh::params::service_enable,
-    Optional[Hash] $manual_package              = undef,
-    Boolean $auto_install                       = $ssh::params::auto_install,
-    String $package_name                        = $ssh::params::package_name,
+    String                                      $service_name,
+    Boolean                                     $auto_install,
+    String                                      $package_name,
+    String                                      $config_file_group,
+    String                                      $config_file_mode,
+    Boolean                                     $manage_package     = true,
+    Boolean                                     $manage_service     = true,
+    Enum["stopped", "running"]                  $service_ensure     = "running",
+    Boolean                                     $service_enable     = true,
+    String                                      $config_file_name   = "/etc/ssh/sshd_config",
+    Optional[Hash]                              $manual_package     = undef,
+    String                                      $service_title      = "sshd",
+    Hash[String,Variant[String,Array[String]]]  $settings           = {},
+) {
 
-    String $config_file_name                    = $ssh::params::config_file_name,
-    String $config_file_group                   = $ssh::params::config_file_group,
-    String $config_file_mode                    = $ssh::params::config_file_mode,
-    String $protocol                            = $ssh::params::protocol,
-    Boolean $manage_protocol                    = true,
-    String $log_level                           = $ssh::params::log_level,
-    Boolean $manage_log_level                   = true,
-    Enum["yes", "no"] $x11_forwarding           = $ssh::params::x11_forwarding,
-    Boolean $manage_x11_forwarding              = true,
-    String $max_auth_tries                      = $ssh::params::max_auth_tries,
-    Boolean $manage_max_auth_tries              = true,
-    Enum["yes", "no"] $ignore_rhosts            = $ssh::params::ignore_rhosts,
-    Boolean $manage_ignore_rhosts               = true,
-    Enum["yes", "no"] $hostbased_authentication = $ssh::params::hostbased_authentication,
-    Boolean $manage_hostbased_authentication    = true,
-    Enum["yes", "no"] $permit_root_login        = $ssh::params::permit_root_login,
-    Boolean $manage_permit_root_login           = true,
-    Enum["yes", "no"] $permit_empty_passwords   = $ssh::params::permit_empty_passwords,
-    Boolean $manage_permit_empty_passwords      = true,
-    Enum["yes", "no"] $permit_user_environment  = $ssh::params::permit_user_environment,
-    Boolean $manage_permit_user_environment     = true,
-    Array[String] $ciphers                      = $ssh::params::ciphers,
-    Boolean $manage_ciphers                     = true,
-    String $client_alive_interval               = $ssh::params::client_alive_interval,
-    Boolean $manage_client_alive_interval       = true,
-    String $client_alive_count_max              = $ssh::params::client_alive_count_max,
-    Boolean $manage_client_alive_count_max      = true,
-    String $banner                              = $ssh::params::banner,
-    Boolean $manage_banner                      = true,
-    Hash $extra_config                          = {},
-) inherits ssh::params {
-
-  $service_title = "sshd"
 
   if $manage_package {
     # must specify all the details for $manual_package if used
@@ -177,86 +125,11 @@ class ssh(
     notify => $notify,
   }
 
-  if $manage_protocol {
-    sshd_config { "Protocol":
-      value => $protocol,
+  $settings.each |$key, $value| {
+    sshd_config { $key:
+      value => $value
     }
   }
-
-  if $manage_log_level {
-    sshd_config { "LogLevel":
-      value => $log_level,
-    }
-  }
-
-  if $manage_x11_forwarding {
-    sshd_config { "X11Forwarding":
-      value => $x11_forwarding,
-    }
-  }
-
-  if $manage_max_auth_tries {
-    sshd_config { "MaxAuthTries":
-      value => $max_auth_tries,
-    }
-  }
-
-  if $manage_ignore_rhosts {
-    sshd_config { "IgnoreRhosts":
-      value => $ignore_rhosts,
-    }
-  }
-
-  if $manage_hostbased_authentication {
-    sshd_config { "HostbasedAuthentication":
-      value => $hostbased_authentication,
-    }
-  }
-
-  if $manage_permit_root_login {
-    sshd_config { "PermitRootLogin":
-      value  => $permit_root_login,
-    }
-  }
-
-  if $manage_permit_empty_passwords {
-    sshd_config { "PermitEmptyPasswords":
-      value  => $permit_empty_passwords,
-    }
-  }
-
-  if $manage_permit_user_environment {
-    sshd_config { "PermitUserEnvironment":
-      value  => $permit_user_environment ,
-    }
-  }
-
-  if $manage_ciphers {
-    sshd_config { "Ciphers":
-      value  => $ciphers,
-    }
-  }
-
-  if $manage_client_alive_interval {
-    sshd_config { "ClientAliveInterval":
-      value  => $client_alive_interval,
-    }
-  }
-
-  if $manage_client_alive_count_max {
-    sshd_config { "ClientAliveCountMax":
-      value  => $client_alive_count_max,
-    }
-  }
-
-  if $manage_banner {
-    sshd_config { "Banner":
-      value  => $banner,
-    }
-  }
-
-  # create any user supplied
-  create_resources("sshd_config", $extra_config)
 
   # we always create Service[sshd] mananging the appropriate service to allow
   # other users of this module to refer to a consistent service name
